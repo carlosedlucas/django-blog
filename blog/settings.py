14c1cacf0270dbdd78f4d6c62a6fc5a8fbfd5d0d
carlosedlucas <carlosedlucas@gmail.com>
2022-12-16 15:42:40 -0300

    Mudando o valor da secret key e não mostrando no git

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%jrx4-wn7n2*z&8brp=2-tq^$t76=86)w#j=w$ifo&d^)^#*2t'
+SECRET_KEY = 'DESCUBRA'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
